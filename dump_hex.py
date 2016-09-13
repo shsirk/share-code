855439f27b11625a953c048628cd319eca85c367
krishs <krishs@krishs-virtualbox.(none)>
2016-09-13 22:59:09 +0530

    some code

---
+++ dump_hex.py
@@ -0,0 +1,30 @@
+
+import sys
+from sys import argv
+
+RED = "\x1B[31m"
+BLK = "\x1B[0m"
+
+def printf(ch):
+    sys.stdout.write(ch)
+    sys.stdout.flush()
+    
+def dump_hex(FILE):
+    with open(FILE, "r") as fd:
+        for line in fd:
+            for ch in line:
+                if (ord(ch) >= 0x20 and ord(ch) <= 0x7e):
+                    printf(BLK)
+                    printf(ch)
+                else:
+                    printf(RED)
+                    printf( ("%02x" % ord(ch)))
+        print ""
+
+
+if __name__ == "__main__":
+    if (len(argv) == 1):
+        print "requires file name"
+    else:
+        dump_hex(argv[1])
+
