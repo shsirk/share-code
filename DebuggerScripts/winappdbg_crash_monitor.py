426d17276320826a63615a94a8e65cda595e13dc
shsirk <shsirk@users.noreply.github.com>
2018-09-06 20:01:29 +0530

    Update winappdbg_crash_monitor.py

--- DebuggerScripts/winappdbg_crash_monitor.py
+++ DebuggerScripts/winappdbg_crash_monitor.py
@@ -48,12 +48,31 @@ def run_within_debugger(args):
   with Debug( crash_handler, bKillOnExit = True ) as debug:
     debug.execv( args , bConsole=True)
     debug.loop()
-    
+
+def run_within_debugger_with_timeout(args):
+  print '[+]    running testcase...'
+  with Debug( crash_handler, bKillOnExit = True ) as debug:
+    debug.execv( args , bConsole=True)
+    System.set_kill_on_exit_mode(True)
+    maxTime = time.time() + 15
+    while debug and time.time() < maxTime:
+      try:
+        debug.wait(1000)  # 1 second accuracy
+      except WindowsError, e:
+        if e.winerror in (win32.ERROR_SEM_TIMEOUT,
+                              win32.WAIT_TIMEOUT):
+          continue
+        raise
+      try:
+        debug.dispatch()
+      finally:
+        debug.cont()
+        
 def download_testcase_and_test():
   while 1:
     print '[+] running next testcase'
     if download_file_to('localhost', 8080, TESTCASE_PATH):
-      run_within_debugger([r"ehx.exe", TESTCASE_PATH])
+      run_within_debugger_with_timeout([r"ehx.exe", TESTCASE_PATH])
     print
     
 if __name__ == "__main__":
